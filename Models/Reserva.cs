04b8ce309ac4b7502d4035a8f5624bc4f252550b
mdlpontual <mdlpontual@gmail.com>
2025-09-09 19:08:50 -0300

    completar desafio base

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -37,25 +37,20 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
+            decimal valorComDesconto = valor - (DiasReservados * Suite.ValorDiaria * 10 / 100);
 
-            return valor;
+            return DiasReservados >= 10 ? valorComDesconto : valor;
+            
         }
     }
 }
